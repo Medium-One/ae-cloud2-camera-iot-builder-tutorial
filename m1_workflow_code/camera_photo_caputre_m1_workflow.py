d0da11feba5ecf4b890d59c5b862b6903bd41b46
Michael C Li <michael.li@medium.one>
2018-11-08 16:15:49 -0800

    update. use device id input instead of s5d9.

--- m1_workflow_code/camera_photo_caputre_m1_workflow.py
+++ m1_workflow_code/camera_photo_caputre_m1_workflow.py
@@ -2,6 +2,6 @@
 import MQTT
 
 # Send a command to the device to take a picture.  Expect to recieve a base64 format string back from the device.
-if (IONode.get_input('in1')['event_data']['value']):
-    MQTT.publish_event_to_client('s5d9', 'C')
+if (IONode.get_input('in2')['event_data']['value']):
+    MQTT.publish_event_to_client(IONode.get_input('in1')['event_data']['value'], 'C')
     log ('Sent the command.')
